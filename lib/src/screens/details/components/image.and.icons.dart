d1d79c5623230f441daa5d05ff485281d7399416
Yan Kaiky <yankaikys@gmail.com>
2022-12-10 17:22:43 -0300

    feat(ImageAndIcons): alter screen

--- lib/src/screens/details/components/image.and.icons.dart
+++ lib/src/screens/details/components/image.and.icons.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop/src/screens/details/components/icon.card.dart';
 import 'package:shop/src/utils/constants.dart';
 
 class ImageAndIcons extends StatelessWidget {
@@ -12,64 +11,80 @@ class ImageAndIcons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: yDefaultPadding * 3),
+    return SizedBox(
+      height: size.height * 0.8,
       child: SizedBox(
-        height: size.height * 0.8,
-        child: Row(
-          children: <Widget>[
+        height: size.height,
+        width: size.width,
+        child: Column(
+          children: [
             Expanded(
               child: Padding(
-                padding: EdgeInsets.symmetric(vertical: yDefaultPadding * 3),
-                child: Column(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: IconButton(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: yDefaultPadding),
-                        icon: Icon(
-                          Icons.arrow_back_rounded,
-                          color: yPrimaryColor,
-                        ),
-                        onPressed: () => Navigator.pop(context),
+                padding: EdgeInsets.symmetric(
+                  vertical: yDefaultPadding * 2,
+                ),
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      child: Image.asset(
+                        'assets/images/air-jordan-1-zoom-confort.png',
                       ),
                     ),
-                    Spacer(),
-                    IconCard(icon: Icons.wb_sunny_outlined),
-                    IconCard(icon: Icons.snowshoeing_sharp),
-                    IconCard(icon: Icons.add_shopping_cart_rounded),
-                    IconCard(icon: Icons.favorite_outline_rounded),
+                    Row(
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: yDefaultPadding,
+                          ),
+                          icon: Icon(
+                            Icons.arrow_back_rounded,
+                            color: yPrimaryColor,
+                          ),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          icon: Icon(
+                            Icons.favorite_outline_rounded,
+                            size: 30,
+                          ),
+                          onPressed: () => print('Add Favorite'),
+                        ),
+                      ],
+                    ),
                   ],
-                ),
-              ),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: yPrimaryColor.withOpacity(0.29),
-                  ),
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image:
-                      AssetImage('assets/images/air-jordan-1-zoom-confort.png'),
                 ),
               ),
             ),
           ],
         ),
       ),
+      // Expanded(
+      //   child: Padding(
+      //     padding: EdgeInsets.symmetric(vertical: yDefaultPadding * 3),
+      //     child: Column(
+      //       children: <Widget>[
+      //         Align(
+      //           alignment: Alignment.topLeft,
+      //           child: IconButton(
+      //             padding:
+      //                 EdgeInsets.symmetric(horizontal: yDefaultPadding),
+      //             icon: Icon(
+      //               Icons.arrow_back_rounded,
+      //               color: yPrimaryColor,
+      //             ),
+      //             onPressed: () => Navigator.pop(context),
+      //           ),
+      //         ),
+      //         Spacer(),
+      //         IconCard(icon: Icons.wb_sunny_outlined),
+      //         IconCard(icon: Icons.snowshoeing_sharp),
+      //         IconCard(icon: Icons.add_shopping_cart_rounded),
+      //         IconCard(icon: Icons.favorite_outline_rounded),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
