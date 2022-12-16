7d084b24ed94524cc4a904f8eea8f18d0a992d84
Yan Kaiky <yankaikys@gmail.com>
2022-12-16 02:35:29 -0300

    feat(screens): add page login

--- lib/src/screens/login/login.screen.dart
+++ lib/src/screens/login/login.screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/login.button.dart';
-import 'package:shop/components/textfield.dart';
 import 'package:shop/src/screens/login/components/customshape.dart';
 import 'package:shop/src/utils/constants.dart';
 
@@ -21,90 +19,149 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ClipPath(
-                    clipper: Customshape(),
-                    child: Container(
-                      height: 300,
-                      color: yPrimaryColor,
-                      child: Stack(
-                        children: [
-                          SizedBox(
+      body: _body(size, context),
+    );
+  }
+
+  SafeArea _body(Size size, BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipPath(
+                  clipper: Customshape(),
+                  child: Container(
+                    height: 300,
+                    color: yPrimaryColor,
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          child: Image.asset(
+                            'assets/images/jordan-1-mid.png',
+                            fit: BoxFit.cover,
+                            width: size.width,
+                          ),
+                        ),
+                        Container(
+                          height: size.height / 2,
+                          color: Colors.black.withOpacity(0.7),
+                          child: Center(
                             child: Image.asset(
-                              'assets/images/jordan-1-mid.png',
-                              fit: BoxFit.cover,
-                              width: size.width,
+                              'assets/images/shop.png',
+                              width: 150,
                             ),
                           ),
-                          Container(
-                            height: size.height / 2,
-                            color: Colors.black.withOpacity(0.7),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/images/shop.png',
-                                width: 150,
-                              ),
-                            ),
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                        color: yTextColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text(
+                  'Sign In to continue',
+                  style: TextStyle(fontSize: 15),
+                ),
+                SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextField(
+                    onChanged: (value) => email = value,
+                    cursorColor: yPrimaryColor,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: yPrimaryColor, //<-- SEE HERE
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 2.0),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
                       ),
                     ),
                   ),
-                  Text(
-                    'Login',
-                    style: Theme.of(context).textTheme.headline4?.copyWith(
-                          color: yTextColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                  Text(
-                    'Sign In to continue',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  SizedBox(height: 50),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: YTextField(
-                      changed: (value) => email = value,
-                      label: 'Email',
-                      size: size,
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextField(
+                    onChanged: (value) => password = value,
+                    obscureText: true,
+                    cursorColor: yPrimaryColor,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: yPrimaryColor, //<-- SEE HERE
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 2.0),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: YTextField(
-                      obscureText: true,
-                      changed: (value) => password = value,
-                      label: 'Password',
-                      size: size,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  LoginButton(email: email, password: password, size: size),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('New User?'),
-                      SizedBox(width: 5),
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: yPrimaryColor,
-                          fontWeight: FontWeight.w600,
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: yDefaultPadding, horizontal: 16.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(yPrimaryColor),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
                       ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                    onPressed: () {
+                      if (email == 'yankaikys@gmail.com' && password == '123') {
+                        Navigator.of(context).pushReplacementNamed('/browse');
+                      } else {
+                        print('Invalid login');
+                      }
+                    },
+                    child: SizedBox(
+                      width: size.width,
+                      height: 50,
+                      child: Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('New User?'),
+                    SizedBox(width: 5),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: yPrimaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
